28ef2d680075b29658f726442fdccb716f76fb91
smerante <merante.s@hotmail.com>
2020-02-09 13:04:27 -0500

    l17: react rendering with styles

--- app1/src/App.js
+++ app1/src/App.js
@@ -1,17 +1,25 @@
 import React, { Component } from 'react';
+import './App.css';
+import * as styles from './styles'
 
 class App extends Component {
-// name = 'same' ==> {this.sam}
+
 
   render() {
-   const name = 'sam'
+
     return (
       <div className="App">
-        React-2019
-       {name}
+        <div style={styles.divStyle}>
+          React-2019
+        <button id="test" className="test">Button 1 </button>
+        </div>
       </div>
     );
   }
 }
+
+const appInstance = new App();
+
+console.warn(appInstance);
 
 export default App;
---
+++ app1/src/styles.js
@@ -0,0 +1,6 @@
+
+export const divStyle = {
+    border: 'solid',
+    textAlign: 'center',
+    boxShadow: '2px 2px'
+}
