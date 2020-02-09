b088e10e4c8a1694cafacabf0d9ed33a8b0af439
smerante <merante.s@hotmail.com>
2020-02-09 13:33:40 -0500

    L19: Rendering objects

--- app1/src/App.js
+++ app1/src/App.js
@@ -2,36 +2,21 @@ import React, { Component } from 'react';
 
 class App extends Component {
 
-  state = {
-    counter: 0
-  };
-
-  increment = () => {
-    this.setState({ counter: 5 })
-  }
-
   render() {
+    const obj1 = {
+      key1: 'Some Data'
+    };
 
     return (
       <div className="App">
-        <button onClick={() => this.increment()}>Button </button>
-        <div>
-          Div 1 {this.state.counter}
-        </div>
-        <div>
-          Div 2
-        </div>
-        <div>
-          Div 3
-        </div>
-
+        { React.createElement(
+          'div',
+          {className: 'app'},
+          obj1.key1
+        ) }
       </div>
     );
   }
 }
-
-const appInstance = new App();
-
-console.warn(appInstance);
 
 export default App;
