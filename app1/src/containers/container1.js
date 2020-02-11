0669aef24ad36a1db8fb371f034dfd2d9f3b5688
smerante <merante.s@hotmail.com>
2020-02-10 19:51:40 -0500

    setup action creator

--- app1/src/containers/container1.js
+++ app1/src/containers/container1.js
@@ -13,10 +13,13 @@ class Container1 extends Component {
         <button onClick={() => console.warn(this.props)}>Get State</button>
         <button onClick={() => this.props.action1()}>Dispatch Action 1</button>
         <button onClick={() => this.props.action2()}>Dispatch Action 2</button>
+        
+        <button onClick={() => this.props.action_creator1()}>Dispatch Action Creator 1</button>
+        <button onClick={() => this.props.action_creator2()}>Dispatch Action Creator 2</button>
+
       </div>
     )
   }
-
 }
 
 function mapStateToProps(state) {
@@ -28,7 +31,9 @@ function mapStateToProps(state) {
 function mapDispatchToProps(dispatch) {
   return {
     action1: () => dispatch(ACTIONS.SUCCESS),
-    action2: () => dispatch(ACTIONS.FAILURE)
+    action2: () => dispatch(ACTIONS.FAILURE),
+    action_creator1: () => dispatch(ACTIONS.success()),
+    action_creator2: () => dispatch(ACTIONS.failure())
   }
 }
 
--- app1/src/store/actions/actions.js
+++ app1/src/store/actions/actions.js
@@ -7,3 +7,15 @@ export const SUCCESS = {
 export const FAILURE = {
 	type: ACTION_TYPES.FAILURE
 }
+
+export const success = () => {
+	return {
+		type: ACTION_TYPES.SUCCESS
+	}
+}
+
+export const failure = () => {
+	return {
+		type: ACTION_TYPES.FAILURE
+	}
+}
