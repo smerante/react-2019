8beedc764624ca8dc270b2cf3b68a4b8300b043d
smerante <merante.s@hotmail.com>
2020-03-15 10:23:31 -0400

    useState hook

--- project/src/hooks/hooks_container1.js
+++ project/src/hooks/hooks_container1.js
@@ -1,12 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 
 //main app 
 const HooksContainer1 = () => {
+
+    // const stateValue = useState(0)[0]
+    // const setValue = useState(0)[0]
+    const [counter, setCounter] = useState(10)
+
+    const incrementValue = () => {
+        setCounter(counter + 1);
+    }
+
+    const decrementValue = () => {
+        setCounter(counter - 1);
+    }
+
     return (
-      <div>
-        React Hooks
-      </div>
+        <div>
+            React Hooks
+            <br />
+            <button onClick={() => incrementValue()}>Incr local state </button>
+            <button onClick={() => decrementValue()}>Decr local value </button>
+
+            <p>Local State: {counter}</p>
+
+        </div>
     )
 }
 
