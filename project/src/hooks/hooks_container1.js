06e43dc70ce6851eca0c705100cae719aa3706b4
smerante <merante.s@hotmail.com>
2020-03-15 10:41:38 -0400

    use reducer hooks

--- project/src/hooks/hooks_container1.js
+++ project/src/hooks/hooks_container1.js
@@ -1,5 +1,6 @@
-import React, { useState, useEffect } from 'react';
-
+import React, { useState, useEffect, useReducer } from 'react';
+import * as Reducer from '../store/hooks_state/hooks-reducer';
+import * as ACTIONS from '../store/actions/actions';
 
 //main app 
 const HooksContainer1 = () => {
@@ -8,7 +9,7 @@ const HooksContainer1 = () => {
     // const setValue = useState(0)[0]
     const [counter, setCounter] = useState(10)
     const [useEffectValue, setUseEffectValue] = useState(null)
-
+    const [hooksState, dispatch] = useReducer(Reducer.HooksReducer, Reducer.initialState)
 
     useEffect(() => {
         setUseEffectValue('useEffect worked!')
@@ -28,13 +29,28 @@ const HooksContainer1 = () => {
         setUseEffectValue('Some string')
     }
 
+    const handleDispatchTrue = () => {
+        // dispatch(ACTIONS.SUCCESS)
+        // dispatch(type: "SUCCESS")
+        dispatch(ACTIONS.success())
+    }
+
+    const handleDispatchFalse = () => {
+        // dispatch(ACTIONS.FAILURE)
+        // dispatch(type: "FAILURE")
+        dispatch(ACTIONS.failure())
+    }
+
     return (
         <div>
             React Hooks
             <br />
-            <button onClick={() => incrementValue()}>Incr local state </button>
-            <button onClick={() => decrementValue()}>Decr local value </button>
-            <button onClick={() => changeUseEffectValue()}>Change useEffect value </button>
+            <button style={{margin: '5px'}} onClick={() => incrementValue()}>Incr local state </button>
+            <button style={{margin: '5px'}} onClick={() => decrementValue()}>Decr local value </button>
+            <button style={{margin: '5px'}} onClick={() => changeUseEffectValue()}>Change useEffect value </button>
+
+            <button style={{margin: '5px'}} onClick={() => handleDispatchTrue()}>Dispatch True </button>
+            <button style={{margin: '5px'}} onClick={() => handleDispatchFalse()}>Dispatch False </button>
 
             <p>Local State: {counter}</p>
             <br />
@@ -45,6 +61,14 @@ const HooksContainer1 = () => {
                         <p>No Value </p>
                 }
             </p>
+
+            <br />
+
+            {
+                hooksState.stateprop1 ?
+                    <p>state prop 1: true</p> :
+                    <p>state prop 1: false </p>
+            }
 
         </div>
     )
