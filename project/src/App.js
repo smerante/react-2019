a5b089fa1c0f5906c8655b5c0d710bdb882681bf
smerante <merante.s@hotmail.com>
2020-03-15 10:14:20 -0400

    add starter project

---
+++ project/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Routes from './routes';
+
+//main app 
+const App = () => {
+    return (
+      <div>
+        React
+        <Routes />
+      </div>
+    )
+}
+
+
+export default App;
---
+++ project/src/hooks/hooks_container1.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+//main app 
+const HooksContainer1 = () => {
+    return (
+      <div>
+        React Hooks
+      </div>
+    )
+}
+
+
+export default HooksContainer1;
